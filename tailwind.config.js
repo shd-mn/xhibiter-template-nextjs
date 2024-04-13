7433c16e7fd7adfd5f7d31e8e421641a89b8f9c3
Shadman <shadman.alizada@gmail.com>
2024-04-13 23:57:41 +0400

    chore: configure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class',
     content: [
         './pages/**/*.{js,ts,jsx,tsx,mdx}',
         './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,7 +8,35 @@ module.exports = {
         './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-        extend: {},
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1234px',
+            },
+        },
+
+        extend: {
+            fontFamily: {
+                calSans: ['var(--font-cal-sans)'],
+                dmSans: ['var(--font-dm-sans)'],
+            },
+            colors: {
+                primary: '#8358fc',
+                'primary-hover': '#8358FF',
+                'text-heading-clr': ' #131740',
+                'text-clr': ' #5A5D79',
+                secondary: '#BDEFFB',
+
+                'black-100': '#161616',
+                'g-accent': '#0066D9',
+                'g-gray': '#474747',
+                'g-light1': '#ECF2FF',
+                'g-light2': '#FBF8EE',
+                'g-light3': '#FFE3EE',
+                'g-shadow': '#c7ceda40',
+            },
+        },
     },
     plugins: [],
 };
