779dde6868f685113b07d0cd4e5b6e751b3aafbd
Shadman <shadman.alizada@gmail.com>
2024-04-15 07:14:15 +0400

    feat: Collection page finished

---
+++ app/collections/page.js
@@ -0,0 +1,6 @@
+import Collections from '@/container/Collections';
+import React from 'react';
+
+export default function CollectionPage() {
+    return <Collections />;
+}
---
+++ container/Collections/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExploreCollections from './ExploreCollections';
+
+function Collections() {
+    return (
+        <main>
+            <ExploreCollections />
+        </main>
+    );
+}
+
+export default Collections;
