72fbc0cf4cd64bb0b6eadbdcfef9964251b6f1be
Shadman <shadman.alizada@gmail.com>
2024-04-15 06:15:49 +0400

    feat: TrendingCategories section created

---
+++ container/Home/TrendingCategories.jsx
@@ -0,0 +1,108 @@
+'use client';
+import Image from 'next/image';
+import { useState } from 'react';
+import lightImg from '@/public/images/26a1.png';
+import {
+    trendingCategoriesBtns,
+    trendingCategoriesData,
+} from './data/trendingCategoriesData';
+import { CheckIcon, ChevronIcon } from '@/data/icons';
+import Checkbox from '@/components/UI/Checkbox';
+import CategoryCard from '@/components/UI/CategoryCard';
+function TrendingCategories() {
+    const [show, setShow] = useState(false);
+
+    return (
+        <section className="py-24">
+            <div className="container">
+                <header className="mb-8">
+                    <div className="mb-8 flex items-center justify-center gap-4">
+                        <Image src={lightImg} alt="" className="h-6 w-6" />
+                        <h2 className="text-center text-3xl dark:text-white ">
+                            Trending categories
+                        </h2>
+                    </div>
+                    <div className="flex flex-wrap items-center justify-between gap-4">
+                        <ul className="flex flex-wrap items-center gap-3">
+                            {trendingCategoriesBtns.map((Item, idx) => (
+                                <li key={idx}>
+                                    <button
+                                        type="button"
+                                        className={`flex items-center gap-1 rounded-lg border px-3 py-[6px] font-calSans text-sm text-t-clr duration-200 hover:bg-primary-hover hover:text-white ${Item.name === 'All' && 'bg-slate-200'}`}
+                                    >
+                                        {Item.icon && (
+                                            <span>
+                                                <Item.icon className="h-4 w-4" />
+                                            </span>
+                                        )}
+                                        <span className="pt-[2px]">
+                                            {Item.name}
+                                        </span>
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+
+                        <div className="relative">
+                            <button
+                                type="button"
+                                className="flex items-center gap-1 rounded-lg border px-3 py-[6px] font-calSans text-sm text-t-clr"
+                                onClick={() => setShow(!show)}
+                            >
+                                <span className="pt-[2px]">
+                                    Price: Low to High
+                                </span>
+                                <span className="ml-8">
+                                    <ChevronIcon
+                                        className={`h-5 w-5 ${show && 'rotate-180'}`}
+                                    />
+                                </span>
+                            </button>
+                            <div
+                                className={`${show ? 'flex' : 'hidden'} absolute top-full z-20 w-full flex-col gap-2 rounded-md bg-white px-2 py-4 shadow-lg`}
+                            >
+                                <span className="px-3 font-calSans text-sm text-t-clr">
+                                    Sort By
+                                </span>
+                                <button
+                                    type="button"
+                                    className="flex w-full items-center justify-between rounded-lg px-3 py-1 text-start font-calSans text-sm  duration-200 hover:bg-gray-100"
+                                >
+                                    <span className="pt-[2px]">
+                                        Price: Low to High
+                                    </span>
+                                    <span>
+                                        <CheckIcon className="h-4 w-4 text-primary-hover" />
+                                    </span>
+                                </button>
+                                <button
+                                    type="button"
+                                    className="flex w-full items-center justify-between rounded-lg px-3 py-1 text-start font-calSans text-sm duration-200 hover:bg-gray-100"
+                                >
+                                    <span className="pt-[2px] text-slate-600">
+                                        Price: High to Low
+                                    </span>
+                                </button>
+                                <span className="px-3 font-calSans text-sm text-t-clr">
+                                    Options
+                                </span>
+
+                                <Checkbox title="Verified Only" />
+                                <Checkbox title="NFSW Only" />
+                                <Checkbox title="Show Lazy Minted" />
+                            </div>
+                        </div>
+                    </div>
+                </header>
+
+                <div className="grid gap-6 sm:grid-cols-2 md:gap-8 lg:grid-cols-4">
+                    {trendingCategoriesData.map((item) => (
+                        <CategoryCard key={item.id} item={item} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default TrendingCategories;
---
+++ container/Home/data/trendingCategoriesData.js
@@ -0,0 +1,156 @@
+import {
+    ArtIcon,
+    CollectIcon,
+    DomainIcon,
+    MusicIcon,
+    PhotographyIcon,
+    VirtualIcon,
+} from '@/data/icons';
+
+import img1 from '@/public/images/item_7.jpg';
+import img2 from '@/public/images/item_10.jpg';
+import img3 from '@/public/images/item_9.jpg';
+import img4 from '@/public/images/item_11.gif';
+import img5 from '@/public/images/item_8.jpg';
+import img6 from '@/public/images/item_4.jpg';
+import img7 from '@/public/images/item_5.jpg';
+import img8 from '@/public/images/item_6.jpg';
+
+import creator1 from '@/public/images/creator_2.png';
+import creator2 from '@/public/images/creator_3.png';
+import creator3 from '@/public/images/creator_6.png';
+import creator4 from '@/public/images/creator_8.png';
+import owner1 from '@/public/images/owner_3.png';
+import owner2 from '@/public/images/owner_4.png';
+import owner3 from '@/public/images/owner_5.png';
+import owner4 from '@/public/images/owner_7.png';
+
+export const trendingCategoriesBtns = [
+    {
+        name: 'All',
+        icon: '',
+    },
+    {
+        name: 'Art',
+        icon: ArtIcon,
+    },
+    {
+        name: 'Collectibles',
+        icon: CollectIcon,
+    },
+    {
+        name: 'Domain',
+        icon: DomainIcon,
+    },
+    {
+        name: 'Music',
+        icon: MusicIcon,
+    },
+    {
+        name: 'Photography',
+        icon: PhotographyIcon,
+    },
+    {
+        name: 'Virtual World',
+        icon: VirtualIcon,
+    },
+];
+
+export const trendingCategoriesData = [
+    {
+        id: 1,
+        name: 'SwagFox#133',
+        price: '0.078 ETH 1/3',
+        image: img1,
+        creator: creator1,
+        creatorLink: '#!',
+        owner: owner1,
+        ownerLink: '#!',
+        like: 160,
+        href: '#!',
+    },
+    {
+        id: 2,
+        name: 'Artof Eve',
+        price: '0.13 ETH 1/1',
+        image: img2,
+        creator: creator2,
+        creatorLink: '#!',
+        owner: owner2,
+        ownerLink: '#!',
+        like: 55,
+        href: '#!',
+    },
+    {
+        id: 3,
+        name: 'Jedidia#149',
+        price: '0.16 ETH 1/1',
+        image: img3,
+        creator: creator3,
+        creatorLink: '#!',
+        owner: owner3,
+        ownerLink: '#!',
+        like: 25,
+        href: '#!',
+    },
+    {
+        id: 4,
+        name: 'Asuna #1649',
+        price: '0.8 ETH 1/1',
+        image: img4,
+        creator: creator4,
+        creatorLink: '#!',
+        owner: owner4,
+        ownerLink: '#!',
+        like: 70,
+        href: '#!',
+    },
+    {
+        id: 5,
+        name: 'Monkeyme#155',
+        price: '0.13 ETH 1/1',
+        image: img5,
+        creator: creator2,
+        creatorLink: '#!',
+        owner: owner4,
+        ownerLink: '#!',
+        like: 55,
+        href: '#!',
+    },
+    {
+        id: 6,
+        name: 'Amazing NFT art',
+        price: '0.8 ETH 1/1',
+        image: img6,
+        creator: creator1,
+        creatorLink: '#!',
+        owner: owner2,
+        ownerLink: '#!',
+        like: 70,
+        href: '#!',
+    },
+    {
+        id: 7,
+        name: 'Flourishing Cat #180',
+        price: '0.078 ETH 1/3',
+        image: img7,
+        creator: creator4,
+        creatorLink: '#!',
+        owner: owner3,
+        ownerLink: '#!',
+        like: 160,
+        href: '#!',
+    },
+    {
+        id: 8,
+        name: 'Splendid Girl',
+        price: '0.16 ETH 1/1',
+        image: img8,
+        creator: creator3,
+        creatorLink: '#!',
+        owner: owner1,
+        ownerLink: '#!',
+        like: 25,
+        href: '#!',
+    },
+];
