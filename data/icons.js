f7ef83b5e6e150ef2915d4cb80373c20105a4f4c
Shadman <shadman.alizada@gmail.com>
2024-04-15 06:23:02 +0400

    feat: icons added

---
+++ data/icons.js
@@ -0,0 +1,243 @@
+export const EthIcon = ({ className }) => {
+    return (
+        <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0"
+            y="0"
+            viewBox="0 0 1920 1920"
+            className={className}
+        >
+            <path fill="#8A92B2" d="M959.8 80.7L420.1 976.3 959.8 731z"></path>
+            <path
+                fill="#62688F"
+                d="M959.8 731L420.1 976.3l539.7 319.1zm539.8 245.3L959.8 80.7V731z"
+            ></path>
+            <path
+                fill="#454A75"
+                d="M959.8 1295.4l539.8-319.1L959.8 731z"
+            ></path>
+            <path fill="#8A92B2" d="M420.1 1078.7l539.7 760.6v-441.7z"></path>
+            <path fill="#62688F" d="M959.8 1397.6v441.7l540.1-760.6z"></path>
+        </svg>
+    );
+};
+export const HeartIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+        >
+            <path fill="none" d="M0 0H24V24H0z"></path>
+            <path d="M12.001 4.529c2.349-2.109 5.979-2.039 8.242.228 2.262 2.268 2.34 5.88.236 8.236l-8.48 8.492-8.478-8.492c-2.104-2.356-2.025-5.974.236-8.236 2.265-2.264 5.888-2.34 8.244-.228zm6.826 1.641c-1.5-1.502-3.92-1.563-5.49-.153l-1.335 1.198-1.336-1.197c-1.575-1.412-3.99-1.35-5.494.154-1.49 1.49-1.565 3.875-.192 5.451L12 18.654l7.02-7.03c1.374-1.577 1.299-3.959-.193-5.454z"></path>
+        </svg>
+    );
+};
+export const CheckIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+        >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+        </svg>
+    );
+};
+export const ArtIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 25 28"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+        >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M12 2c5.522 0 10 3.978 10 8.889a5.558 5.558 0 0 1-5.556 5.555h-1.966c-.922 0-1.667.745-1.667 1.667 0 .422.167.811.422 1.1.267.3.434.689.434 1.122C13.667 21.256 12.9 22 12 22 6.478 22 2 17.522 2 12S6.478 2 12 2zm-1.189 16.111a3.664 3.664 0 0 1 3.667-3.667h1.966A3.558 3.558 0 0 0 20 10.89C20 7.139 16.468 4 12 4a8 8 0 0 0-.676 15.972 3.648 3.648 0 0 1-.513-1.86z"></path>
+        </svg>
+    );
+};
+export const CollectIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 25 28"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+        >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M2 4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v5.5a2.5 2.5 0 1 0 0 5V20a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4zm6.085 15a1.5 1.5 0 0 1 2.83 0H20v-2.968a4.5 4.5 0 0 1 0-8.064V5h-9.085a1.5 1.5 0 0 1-2.83 0H4v14h4.085zM9.5 11a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"></path>
+        </svg>
+    );
+};
+export const DomainIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 25 28"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+        >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M5 15v4h4v2H3v-6h2zm16 0v6h-6v-2h4v-4h2zm-8.001-9l4.4 11h-2.155l-1.201-3h-4.09l-1.199 3H6.6l4.399-11h2zm-1 2.885L10.752 12h2.492l-1.245-3.115zM9 3v2H5v4H3V3h6zm12 0v6h-2V5h-4V3h6z"></path>
+        </svg>
+    );
+};
+export const MusicIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 25 28"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+        >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M12 13.535V3h8v3h-6v11a4 4 0 1 1-2-3.465z"></path>
+        </svg>
+    );
+};
+export const PhotographyIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 25 28"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+        >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M2 6c0-.552.455-1 .992-1h18.016c.548 0 .992.445.992 1v14c0 .552-.455 1-.992 1H2.992A.994.994 0 0 1 2 20V6zm2 1v12h16V7H4zm10 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 2a5 5 0 1 1 0-10 5 5 0 0 1 0 10zM4 2h6v2H4V2z"></path>
+        </svg>
+    );
+};
+
+export const VirtualIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 25 28"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+        >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-2.29-2.333A17.9 17.9 0 0 1 8.027 13H4.062a8.008 8.008 0 0 0 5.648 6.667zM10.03 13c.151 2.439.848 4.73 1.97 6.752A15.905 15.905 0 0 0 13.97 13h-3.94zm9.908 0h-3.965a17.9 17.9 0 0 1-1.683 6.667A8.008 8.008 0 0 0 19.938 13zM4.062 11h3.965A17.9 17.9 0 0 1 9.71 4.333 8.008 8.008 0 0 0 4.062 11zm5.969 0h3.938A15.905 15.905 0 0 0 12 4.248 15.905 15.905 0 0 0 10.03 11zm4.259-6.667A17.9 17.9 0 0 1 15.973 11h3.965a8.008 8.008 0 0 0-5.648-6.667z"></path>
+        </svg>
+    );
+};
+
+export const ChevronIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+            stroke="currentColor"
+        >
+            {/* <path fill="none" d="M0 0h24v24H0z"></path> */}
+            <path d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z"></path>
+        </svg>
+    );
+};
+export const HisotryIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+        >
+            <path fill="none" d="M0 0H24V24H0z"></path>
+            <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12h2c0 4.418 3.582 8 8 8s8-3.582 8-8-3.582-8-8-8C9.25 4 6.824 5.387 5.385 7.5H8v2H2v-6h2V6c1.824-2.43 4.729-4 8-4zm1 5v4.585l3.243 3.243-1.415 1.415L11 12.413V7h2z"></path>
+        </svg>
+    );
+};
+
+export const MenuIcon = ({ className }) => {
+    return (
+        <svg
+            width="16"
+            height="4"
+            viewBox="0 0 16 4"
+            xmlns="http://www.w3.org/2000/svg"
+            className={className}
+            fill="currentColor"
+        >
+            <circle cx="2" cy="2" r="2"></circle>
+            <circle cx="8" cy="2" r="2"></circle>
+            <circle cx="14" cy="2" r="2"></circle>
+        </svg>
+    );
+};
+
+export const ImgCollectIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+        >
+            <path fill="none" d="M0 0h24v24H0z" />
+            <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z" />
+        </svg>
+    );
+};
+
+export const NftIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+        >
+            <path fill="none" d="M0 0h24v24H0z" />
+            <path d="M17.409 19c-.776-2.399-2.277-3.885-4.266-5.602A10.954 10.954 0 0 1 20 11V3h1.008c.548 0 .992.445.992.993v16.014a1 1 0 0 1-.992.993H2.992A.993.993 0 0 1 2 20.007V3.993A1 1 0 0 1 2.992 3H6V1h2v4H4v7c5.22 0 9.662 2.462 11.313 7h2.096zM18 1v4h-8V3h6V1h2zm-1.5 9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+        </svg>
+    );
+};
+
+export const SaleIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            className={className}
+            fill="currentColor"
+        >
+            <path fill="none" d="M0 0h24v24H0z" />
+            <path d="M10.9 2.1l9.899 1.415 1.414 9.9-9.192 9.192a1 1 0 0 1-1.414 0l-9.9-9.9a1 1 0 0 1 0-1.414L10.9 2.1zm2.828 8.486a2 2 0 1 0 2.828-2.829 2 2 0 0 0-2.828 2.829z" />
+        </svg>
+    );
+};
+
